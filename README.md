3f7d4bf9a5da7ff3eacd2e6ff274a8292380d3b0
Yasser Carreón <yacasia@gmail.com>
2020-01-21 14:57:26 -0600

    catch errors in a js promise chain with catch

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Course > [egghead](https://egghead.io/courses/javascript-promises-in-depth)
 ### Lessons:
 
 1.  create a promise chain in javascript with promise then
+2.  catch errors in a javascript promise chain with promise prototype catch
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const API_URL = "https://starwars.egghead.training/";
+const WRONG_API_URL = "https://star-wars.egghead.training/";
 
-const responsePromise = fetch(API_URL + "films");
+const responsePromise = fetch(WRONG_API_URL + "films");
+const responsePromise2 = fetch(API_URL + "films");
 
 const output = document.getElementById("output");
+const output2 = document.getElementById("output2");
 
 output.innerText = "Loading...";
 
-console.log('fetch(API_URL + "films")', responsePromise);
+// console.log('fetch(API_URL + "films")', responsePromise);
 
 function getMovieTitles(films) {
   return films
@@ -15,22 +18,81 @@ function getMovieTitles(films) {
     .join("\n");
 }
 
-responsePromise
-  .then(response => {
+responsePromise.then(
+  response => {
     console.log("response >", response);
-
     const json = response.json();
-
     console.log("response.json() >", json);
+    return json.then(films => {
+      console.log("films >", films);
+      const movieTitles = getMovieTitles(films);
+      console.log("getMovieTitles() >", movieTitles);
+      output.innerText = movieTitles;
+    });
+  },
+  error => {
+    console.warn(error);
+    output.innerText = ":(";
+  }
+);
+//   .then(films => {
+//     console.log("films >", films);
+//     const movieTitles = getMovieTitles(films);
+//     console.log("getMovieTitles() >", movieTitles);
+//     output.innerText = movieTitles;
+//   });
 
-    return json;
+responsePromise2.then(
+  response => {
+    return Promise.reject("Invalid JSON").then(
+      films => {
+        output2.innerText = getMovieTitles(films);
+      },
+      error => {
+        console.warn(error);
+        output2.innerText = ":(";
+      }
+    );
+  },
+  error => {
+    console.warn(error);
+    output.innerText = ":(";
+  }
+);
+
+responsePromise2
+  .then(response => {
+    return Promise.reject("Invalid JSON 2").then(films => {
+      output2.innerText = getMovieTitles(films);
+    });
   })
-  .then(films => {
-    console.log("films >", films);
+  .then(undefined, error => {
+    console.warn(error);
+    output.innerText = ":(";
+  });
 
-    const movieTitles = getMovieTitles(films);
+responsePromise2
+  .then(response => {
+    return Promise.reject("Invalid JSON 3").then(films => {
+      output2.innerText = getMovieTitles(films);
+    });
+  })
+  .catch(error => {
+    console.warn(error);
+    output.innerText = ":(";
+  });
 
-    console.log("getMovieTitles() >", movieTitles);
+fetch(API_URL + "movies") // Even if the endpoint doesn't exist (404) the promise will be fullfilled
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("unsuccessful response");
+    }
 
-    output.innerText = movieTitles;
+    return response.json().then(films => {
+      output2.innerText = getMovieTitles(films);
+    });
+  })
+  .catch(error => {
+    console.warn(error);
+    output.innerText = ":(";
   });
